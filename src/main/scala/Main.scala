308ad5f5a8baa8b690645f3f22580db2da9dd160
P10r10 <asantiago@keemail.me>
2023-03-15 16:30:58 +0000

    1.1

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,12 +6,24 @@ object Main {
   //  (&&) and logical disjunction (||) operators could be applied to a boolean List.
   //    Write methods to represent these behaviors using
   //    i) foldRight
+
+  def concat[A](lst1: List[A], lst2: List[A]):List[A] = (lst1 foldRight lst2) ((h, t) => h :: t)
+
+  def concat2[A](lst1: List[A], lst2: List[A]):List[A] = lst1 match {
+    case Nil => lst2
+    case h :: t => h :: concat2(t, lst2)
+  }
+
   //    ii) foldLeft
-  def my_sum(lst: List[Int]):Int = (lst foldRight 1) (_ * _)
+
+  def concatFL[A](lst1: List[A], lst2: List[A]):List[A] = (lst2 foldLeft  lst1) ((h, t) => h :+ t)
 
   def main(args: Array[String]): Unit = {
-    val lst = List(1, 2, 3, 4, 5)
+    val lst1 = List(1, 2, 3, 4, 5)
+    val lst2 = List(7, 8, 9, 10, 11)
+    val lst3 = List(true, true)
+    val lst4 = List(false, false)
 
-    println(my_sum(lst))
+    println(concatFL(lst1, lst2))
   }
 }
