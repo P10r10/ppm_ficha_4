437da6832af5cbda1cc832ec9e81c2fc4bc36cc7
P10r10 <asantiago@keemail.me>
2023-03-15 12:16:40 +0000

    preparing exercises

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,17 @@
 object Main {
+
+  //  Exercise 1
+  //
+  //  1.1.The concatenation operator(++) can be applied to generic Lists and both logical conjunction
+  //  (&&) and logical disjunction (||) operators could be applied to a boolean List.
+  //    Write methods to represent these behaviors using
+  //    i) foldRight
+  //    ii) foldLeft
+  def my_sum(lst: List[Int]):Int = (lst foldRight 1) (_ * _)
+
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    val lst = List(1, 2, 3, 4, 5)
+
+    println(my_sum(lst))
   }
 }
